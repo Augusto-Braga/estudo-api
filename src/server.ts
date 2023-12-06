b5c9e2c9a77763e43fdaf88acbb3f4d1b2c747a6
Augusto Braga <bragaaugusto361@gmail.com>
2023-12-06 19:07:22 -0300

    aula 010: fastify plugins

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-console.log(env.PORT)
-console.log('aaaa')
-
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 500)
-    .select('*')
-
-  return transactions
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
